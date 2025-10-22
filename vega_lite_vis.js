83dd5f3d605355a68d80a1f9a4854bba8fc64e19
nzum0001 <nzum0001@student.monash.edu>
2025-10-22 14:04:09 +0800

    Uploading all the files

---
+++ vega_lite_vis.js
@@ -0,0 +1,58 @@
+var vg_1 = "chart1.json";
+vegaEmbed("#line_chart_1", vg_1)
+  .then(function(result) {
+      // chart loaded
+  })
+  .catch(console.error);
+
+var vg_2 = "chart2.json";
+vegaEmbed("#stacked_chart", vg_2)
+  .then(function(result) {
+      // chart loaded
+  })
+  .catch(console.error);
+
+var vg_3 = "chart3.json";
+vegaEmbed("#scatter_plot", vg_3)
+  .then(function(result) {
+      // chart loaded
+  })
+  .catch(console.error);
+
+var vg_4 = "chart4.json";
+vegaEmbed("#line_chart_2", vg_4)
+  .then(function(result) {
+      // chart loaded
+  })
+  .catch(console.error);
+
+
+
+var vg_5 = "chart5.json";
+vegaEmbed("#pie_chart", vg_5)
+  .then(function(result) {
+      // chart loaded
+  })
+  .catch(console.error);
+
+
+var vg_6 = "chart6.json";
+vegaEmbed("#bar_chart", vg_6)
+  .then(function(result) {
+      // chart loaded
+  })
+  .catch(console.error);
+
+var vg_7 = "chart7.json";
+vegaEmbed("#lolli_pop_chart", vg_7)
+  .then(function(result) {
+      // chart loaded
+  })
+  .catch(console.error);
+
+var vg_8 = "map8.json";
+vegaEmbed("#choropleth_map", vg_8)
+  .then(function(result) {
+      // map loaded
+  })
+  .catch(console.error);
